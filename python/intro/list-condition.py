2d49f897db2bf31df6a5cf47aa02a3aaf68d5fd2
Jason Pell <jason@pellcorp.com>
2022-10-24 18:30:04 +1100

    add preso for tomorrow

--- python/intro/list-condition.py
+++ python/intro/list-condition.py
@@ -2,16 +2,6 @@ names = ['jason', 'thomas', 'zachary', 'ella', 'clair', 'lucy']
 
 if 'jason' in names:
     print("I'm Dad")
-elif 'thomas' in names:
-    print("I'm Thomas")
-elif 'zachary' in names:
-    print("I'm Thomas")
-elif 'ella' in names:
-    print("I'm the dog")
-elif 'clair' in names:
-    print("I'm Mum")
-else:
-    print("I'm the Cat!")
 
 if 'thor' not in names:
     print("I'm not thor!")
