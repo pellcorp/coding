d3cefe3c4e693b434266845ddf0bbe0be5f37e19
Jason Pell <jason@pellcorp.com>
2022-10-31 12:32:41 +1100

    added a web site for python code

--- generate-html-sources.sh
+++ generate-html-sources.sh
@@ -6,10 +6,17 @@ start='<html><body><pre>'
 end='</pre></body></html>'
 
 pushd $CURRENT_DIR > /dev/null
-files=$(find . -name "*.py")
+
+index_start='<html><body><ul>'
+index_end='</ul></body></html>'
+
+find docs -name "index.html" -exec rm {} \;
+
+files=$(find . -name "*.py" | sort)
 for file in $files; do
   dirname=$(dirname $file | sed 's/\.\///g')
-  name=$(basename $file).html
+  basename=$(basename $file)
+  name=${basename}.html
 
   echo "Writing: docs/$dirname/$name"
   mkdir -p docs/$dirname
@@ -17,4 +24,19 @@ for file in $files; do
   echo $start > "docs/$dirname/$name"
   cat $file >> "docs/$dirname/$name"
   echo $end >> "docs/$dirname/$name"
+
+  if [ ! -f docs/$dirname/index.html ]; then
+    echo "$index_start" > docs/$dirname/index.html
+  fi
+  echo "<li><a href=\"$name\">$basename</a></li>" >> docs/$dirname/index.html
 done
+
+pushd docs/python > /dev/null
+dirs=$(find . -name "index.html" -exec dirname {} \; | sort | uniq)
+
+echo "$index_start" > index.html
+for dir in $dirs; do
+  echo "$index_end" >> $dir/index.html
+  echo "<li><a href=\"$dir\">$(basename $dir)</a></li>" >> index.html
+done
+echo "$index_end" >> index.html
