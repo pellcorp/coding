a67297896c4db4dee6c93ba4aca648d1f7c629c7
Jason Pell <jason@pellcorp.com>
2022-10-31 12:41:46 +1100

    some fixes

--- generate-html-sources.sh
+++ generate-html-sources.sh
@@ -2,13 +2,7 @@
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd -P)"
 
-start='<html><body><pre>'
-end='</pre></body></html>'
-
 pushd $CURRENT_DIR > /dev/null
-
-index_start='<html><body><ul>'
-index_end='</ul></body></html>'
 
 find docs -name "index.html" -exec rm {} \;
 
@@ -20,23 +14,62 @@ for file in $files; do
 
   echo "Writing: docs/$dirname/$name"
   mkdir -p docs/$dirname
-
-  echo $start > "docs/$dirname/$name"
+  echo '<html><body><pre>' > "docs/$dirname/$name"
   cat $file >> "docs/$dirname/$name"
-  echo $end >> "docs/$dirname/$name"
-
-  if [ ! -f docs/$dirname/index.html ]; then
-    echo "$index_start" > docs/$dirname/index.html
-  fi
+  echo '</pre></body></html>' >> "docs/$dirname/$name"
   echo "<li><a href=\"$name\">$basename</a></li>" >> docs/$dirname/index.html
 done
 
 pushd docs/python > /dev/null
 dirs=$(find . -name "index.html" -exec dirname {} \; | sort | uniq)
 
-echo "$index_start" > index.html
+echo "<html><body><ul>" > index.html
 for dir in $dirs; do
-  echo "$index_end" >> $dir/index.html
+  echo "<html><body>" > $dir/index.html
+  echo "<a href=\"..\">Back</a>" >> $dir/index.html
+  echo "<ul>" >> $dir/index.html
+  files=$(find $dir -type f)
+  for file in $files; do
+    if [ "$(basename $file)" != "index.html" ]; then
+      echo "<li><a href=\"$(basename $file)\">$(basename $file .html)</a></li>" >> $dir/index.html
+    fi
+  done
+
+  # now write the other files
+  files=$(find ../../python/$dir -type f)
+  for file in $files; do
+    extension="${file##*.}"
+    if [ "$extension" != "py" ]; then
+      echo "<li><a href=\"../../../python/$dir/$(basename $file)\">$(basename $file)</a></li>" >> $dir/index.html
+    fi
+  done
+
+  echo "</ul></body></html>" >> $dir/index.html
+
   echo "<li><a href=\"$dir\">$(basename $dir)</a></li>" >> index.html
 done
-echo "$index_end" >> index.html
+echo "</ul></body></html>" >> index.html
+popd > /dev/null
+
+files=()
+while IFS=  read -r -d $'\0'; do
+    files+=("$REPLY")
+done < <(find docs -name "*.pdf" -print0)
+
+echo "<html><body>" > docs/index.html
+
+echo "<a href=\"..\">Back</a>" >> docs/index.html
+echo "<ul>" >> docs/index.html
+echo "<li><a href=\"python\">python</a></li>" >> docs/index.html
+
+for file in "${files[@]}"; do
+  basename=$(basename "$file")
+  echo "<li><a href=\"$basename\">$basename</a></li>" >> docs/index.html
+done
+echo "</ul></body></html>" >> docs/index.html
+
+echo "<html><body><ul>" > index.html
+echo "<li><a href=\"docs\">docs</a></li>" >> index.html
+echo "<li><a href=\"python\">python</a></li>" >> index.html
+echo "<li><a href=\"static\">static</a></li>" >> index.html
+echo "</ul></body></html>" >> index.html
