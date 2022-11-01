060bceda43f0e6edbf0b6b365cd5bc5ff1e7cdd5
Jason Pell <jason@pellcorp.com>
2022-11-01 12:34:50 +1100

    refactored names a bit

---
+++ python/intro/5-decimal-condition.py
@@ -0,0 +1,8 @@
+im_a_decimal = 2.9
+
+if im_a_decimal >= 3.3:
+    print("Im equal to or bigger than 3.3")
+elif im_a_decimal >= 3:
+    print("Im bigger than or equal to 3")
+else:
+    print("Im smaller than 3.0")
