7b9f8545e74b1ed03f5ec3b3f10a64a44462636a
Jason Pell <jason@pellcorp.com>
2022-11-01 12:55:52 +1100

    add some bugs to the int and float samples

--- python/intro/5-float-condition.py
+++ python/intro/5-float-condition.py
@@ -2,7 +2,7 @@ im_a_float = 2.9
 
 if im_a_float >= 3.3:
     print("Im equal to or bigger than 3.3")
-elif im_a_float >= 3:
-    print("Im bigger than or equal to 3")
+elif im_a_float > 3.0:
+    print("Im bigger than 3.0")
 else:
     print("Im smaller than 3.0")
